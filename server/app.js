54ec9196fb4d46d1f84edb91da28361d6fadc4ab
Jayson P <jnp1380@rit.edu>
2025-04-16 13:47:36 -0400

    fixed dotenv error

--- server/app.js
+++ server/app.js
@@ -1,21 +1,23 @@
-require('dotenv').config(); // Load environment variables from .env file
-const path = require('path');
-const express = require('express');
-const compression = require('compression');
-const favicon = require('serve-favicon');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const expressHandlebars = require('express-handlebars');
-const helmet = require('helmet');
-const session = require('express-session');
-const RedisStore = require('connect-redis').default;
-const redis = require('redis');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+} // Load // Load environment variables from .env file
+const path = require("path");
+const express = require("express");
+const compression = require("compression");
+const favicon = require("serve-favicon");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const expressHandlebars = require("express-handlebars");
+const helmet = require("helmet");
+const session = require("express-session");
+const RedisStore = require("connect-redis").default;
+const redis = require("redis");
 
-const router = require('./router.js');
+const router = require("./router.js");
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURI = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+const dbURI = process.env.MONGODB_URI || "mongodb://localhost/DomoMaker";
 mongoose.connect(dbURI).catch((err) => {
   if (err) {
     console.log("couldn't connect to the server");
@@ -26,11 +28,11 @@ const redisClient = redis.createClient({
   url: process.env.REDISCLOUD_URL,
 });
 
-redisClient.on('error', (err) => console.log('Redis Client error:', err));
+redisClient.on("error", (err) => console.log("Redis Client error:", err));
 redisClient.connect().then(() => {
   const app = express();
   app.use(helmet());
-  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
+  app.use("/assets", express.static(path.resolve(`${__dirname}/../hosted`)));
   app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
   app.use(compression());
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -39,17 +41,17 @@ redisClient.connect().then(() => {
   // Set up session management
   app.use(
     session({
-      key: 'sessionId',
+      key: "sessionId",
       store: new RedisStore({ client: redisClient }),
-      secret: 'Domo Arigato',
+      secret: "Domo Arigato",
       resave: false,
       saveUninitialized: false,
-    }),
+    })
   );
 
-  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-  app.set('view engine', 'handlebars');
-  app.set('views', `${__dirname}/../views`);
+  app.engine("handlebars", expressHandlebars.engine({ defaultLayout: "" }));
+  app.set("view engine", "handlebars");
+  app.set("views", `${__dirname}/../views`);
 
   router(app);
 
